4593aaee5b9301a0df50145827a8763fbde63f25
knowOne08 <yashdarji23082004@gmail.com>
2023-02-05 18:15:35 +0530

    added helmet nosniff

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ require('dotenv').config();
 const apiRoutes         = require('./routes/api.js');
 const fccTestingRoutes  = require('./routes/fcctesting.js');
 const runner            = require('./test-runner');
-
+const helmet             = require('helmet'); 
 let app = express();
 
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -18,7 +18,7 @@ app.use(cors({origin: '*'})); //For FCC testing purposes only
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
+app.use(helmet.noSniff);
 //Index page (static HTML)
 app.route('/')
   .get(function (req, res) {
