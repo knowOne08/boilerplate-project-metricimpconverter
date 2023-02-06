ba2c02ffca9bb05aecb182a12f030427608b6002
knowOne08 <yashdarji23082004@gmail.com>
2023-02-06 17:55:52 +0530

    last tests not fucking passing

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -28,7 +28,7 @@ suite('Functional Tests', function() {
       .query({input:"32g"})
       .end(function (err, res) {
         assert.equal(res.status, 200);
-        assert.equal(res.text, '"invalid unit"');
+        assert.equal(res.text, "'invalid unit'");
         done();
       });
   });
@@ -39,7 +39,7 @@ suite('Functional Tests', function() {
       .query({input:"3/7.2/4kg"})
       .end(function (err, res) {
         assert.equal(res.status, 200);
-        assert.equal(res.text, '"invalid number"');
+        assert.equal(res.text, "'invalid number'");
         done();
       });
   });
@@ -50,7 +50,7 @@ suite('Functional Tests', function() {
       .query({input:"3/7.2/4kilomegagram"})
       .end(function (err, res) {
         assert.equal(res.status, 200);
-        assert.equal(res.text, '"invalid number and unit"');
+        assert.equal(res.text, "'invalid number and unit'");
         done();
       });
   });
